aa5382ba602102bb4fb7b71d8ec1fa2a763b4e35
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-06-25 21:15:15 +1000

    Added Todo list and todo components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                },
+                {
+                    id: 2,
+                    text: 'Clean the yard'
+                },
+                {
+                    id: 3,
+                    text: 'Wash dishes'
+                },
+                {
+                    id: 4,
+                    text: 'Check the mail'
+                }
+            ]
+        };
+    },
     render: function () {
+        var {todos} = this.state;
+
         return (
             <div>
-                Todo App
+                <TodoList todos={todos}></TodoList>
             </div>
         )
     }
