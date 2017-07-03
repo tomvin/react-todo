be11e35bd568e9fd2d9d49f5a18e0abec09e9d42
Tom Vinnicombe <tom_vinnicombe@live.com.au>
2017-07-03 19:26:43 +1000

    Added uuid for each todo. Wired up addTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
     getInitialState: function () {
@@ -10,19 +11,19 @@ var TodoApp = React.createClass({
             searchText: '',
             todos: [
                 {
-                    id: 1,
+                    id: uuid(),
                     text: 'Walk the dog'
                 },
                 {
-                    id: 2,
+                    id: uuid(),
                     text: 'Clean the yard'
                 },
                 {
-                    id: 3,
+                    id: uuid(),
                     text: 'Wash dishes'
                 },
                 {
-                    id: 4,
+                    id: uuid(),
                     text: 'Check the mail'
                 }
             ]
@@ -35,16 +36,14 @@ var TodoApp = React.createClass({
         });
     },
     handleAddTodo: function (text) {
-        var currentTodosList = this.state.todos;
-        var newTodoId = currentTodosList.length + 1;
-
-        currentTodosList.push({
-            id: newTodoId,
-            text: text
-        });
-
         this.setState({
-            todos: currentTodosList
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
         });
     },
     render: function () {
